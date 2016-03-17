1547d14324196f092c6fb11d695fede51aad3a29
Paul Mathew <Paul Mathew>
2016-03-18 01:32:03 +0530

    Extended program to print number of files directly under each directory

--- A1.java
+++ A1.java
@@ -2,21 +2,38 @@ import java.io.File;
 
 class A1{
 	
-	public static void PrintingFileList(File maindirectory) { //Function that accepts
-		File[] filelist;								//a file/directory name and	
+	public static void PrintingToScreen (File currentfile, int numberoffiles ){
+		if(numberoffiles == 0){
+			System.out.println (numberoffiles+" f "+currentfile.getAbsolutePath());
+		}
+		else {
+			System.out.println (numberoffiles+" d "+currentfile.getAbsolutePath());
+		}
+	}
+	
+	public static void PrintingFilesInDirectory(File maindirectory) { 
+		int numberoffiles;								//Function that accepts
+		File[] filelist, subdirectoryfiles;				//a file/directory name and	
 		filelist = maindirectory.listFiles();			//prints all the files that
-		for (File currentfile:filelist){				//it contains
-			if (currentfile.isFile()){	
-			System.out.println ("f "+currentfile.getAbsolutePath());
+		for (File currentfile:filelist){				//it contains and the number
+			if (currentfile.isFile()){					//of files as well
+				PrintingToScreen (currentfile, 0);
 			}
 			else{
-				System.out.println ("d "+currentfile.getAbsolutePath());
-				PrintingFileList (currentfile);
+				numberoffiles = 0;
+				subdirectoryfiles = currentfile.listFiles();
+				for(File file:subdirectoryfiles){
+					if (file.isFile()){
+						numberoffiles++;
+					}
+				}
+				PrintingToScreen (currentfile, numberoffiles);
+				PrintingFilesInDirectory (currentfile);
 			}
 		}
 	}
 	public static void main(String[] args){
 		File f = new File(".");
-		PrintingFileList(f);
+		PrintingFilesInDirectory(f);
 	}
 }
