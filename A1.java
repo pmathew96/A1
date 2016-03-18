bc2e0ea0e6a4e2503a2995c3cb0e09e5c8c3023a
Paul Mathew <Paul Mathew>
2016-03-18 16:02:47 +0530

    Fixed program execution time bug and made the number of file types more flexible

--- A1.java
+++ A1.java
@@ -3,17 +3,17 @@ import java.lang.String;
 
 class A1{
 	
-	public static void PrintingNumberOfFilesByType(File currentdirectory){
+	public static void printingNumberOfFilesByType(File currentdirectory){
 		File[] filelist;
 		filelist = currentdirectory.listFiles();
 		String[]TYPES = new String[]{"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml"};
-		int[] filesizecounterarray = new int[9];
-		int[] filetypecounterarray = new int[9];	//Function that takes in the current directory
+		int[] filesizecounterarray = new int[TYPES.length];
+		int[] filetypecounterarray = new int[TYPES.length];	//Function that takes in the current directory
 		int flag, i;						//and prints the number of files by type
 		String filename, filesize;					//and prints the combined size of all files
 		for (File file:filelist){			//of that type
 			filename = (file.getAbsolutePath().toLowerCase());
-			for (i = 0; i < 9; i++){
+			for (i = 0; i < TYPES.length; i++){
 				flag = filename.indexOf (TYPES[i]);
 				if (flag != 1){
 					filetypecounterarray[i]++;
@@ -23,13 +23,13 @@ class A1{
 			}
 		}
 		System.out.println("Number of files by type:");
-		for(i = 0; i < 9; i++){
+		for(i = 0; i < TYPES.length; i++){
 			filesize = String.format("%,d", filesizecounterarray[i]);
 			System.out.println(TYPES[i]+" - "+filetypecounterarray[i]+" "+filesize);
 		}
 	}
 	
-	public static int ComputingTotalFilesInDirectory (File currentfile, int filessofar){
+	public static int computingTotalFilesInDirectory (File currentfile, int filessofar){
 		File[] currentdirectoryfiles;					//Function that takes in the current
 		int numberoffiles = filessofar;			//directory and the files so for in previous directories
 		currentdirectoryfiles = currentfile.listFiles();//and returns the number of files in the current
@@ -38,13 +38,13 @@ class A1{
 				numberoffiles++;
 			}
 			else{
-				numberoffiles += ComputingTotalFilesInDirectory (file, numberoffiles);
+				numberoffiles += computingTotalFilesInDirectory (file, numberoffiles);
 			}
 		}
 		return numberoffiles;
 	}
 	
-	public static void PrintingToScreen (File currentfile, int filesdirectlyunder, int totalfiles ){
+	public static void printingToScreen (File currentfile, int filesdirectlyunder, int totalfiles ){
 		if(filesdirectlyunder == 0 && currentfile.isFile()){
 			System.out.println (totalfiles+" "+ filesdirectlyunder+" f "+currentfile.getAbsolutePath());
 		}
@@ -53,13 +53,13 @@ class A1{
 		}
 	}
 	
-	public static void PrintingFilesInDirectory(File maindirectory) { 
+	public static void printingFilesInDirectory(File maindirectory) { 
 		int numberoffiles, totalfiles;					//Function that accepts
 		File[] filelist, subdirectoryfiles;				//a file/directory name and	
 		filelist = maindirectory.listFiles();			//prints all the files that
 		for (File currentfile:filelist){				//it contains and the number
 			if (currentfile.isFile()){					//of files as well
-				PrintingToScreen (currentfile, 0, 0);
+				printingToScreen (currentfile, 0, 0);
 			}
 			else{
 				numberoffiles = 0;
@@ -69,17 +69,21 @@ class A1{
 						numberoffiles++;
 					}
 				}
-				totalfiles = ComputingTotalFilesInDirectory (currentfile, 0);
-				PrintingToScreen (currentfile, numberoffiles, totalfiles);
-				PrintingFilesInDirectory (currentfile);
+				totalfiles = computingTotalFilesInDirectory (currentfile, 0);
+				printingToScreen (currentfile, numberoffiles, totalfiles);
+				printingFilesInDirectory (currentfile);
 			}
 		}
 	}
 	
 	public static void main(String[] args){
+		long startTime = System.currentTimeMillis();
+		long totalTime;
 		File f = new File(".");
-		PrintingFilesInDirectory(f);
-		PrintingNumberOfFilesByType(f);
-		System.out.println("Program executed in "+System.currentTimeMillis()+" milliseconds");
+		printingFilesInDirectory(f);
+		printingNumberOfFilesByType(f);
+		long stopTime = System.currentTimeMillis();
+		totalTime = stopTime-startTime;
+		System.out.println("Program executed in "+totalTime+" milliseconds");
 	}
 }
